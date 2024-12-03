ddf8c3c72e7abc72dbb98a89f043a3cd587b1dba
JW <kingcoding2298@gmail.com>
2024-12-03 14:35:02 +0900

    [sec3 - I/O 기본1]  - 버퍼 활용
    - 실행 결과의 `BUFFER_SIZE` 는 8192(8KB)이다
    - BUFFER_SIZE` 에 따른 쓰기 성능
    - 버퍼의 크기가 커진다고 해서 속도가 계속 줄어들지는 않는다.
    -  디스크나 파일 시스템에서 데이터를 읽고 쓰는 기본 단위가 보통 4KB 또는 8KB이기 때문이다.
    
    * 4KB (4096byte)
    * 8KB (8192byte)

---
+++ workspace/java-adv2/src/sec3/io/buffered/CreateFileV2.java
@@ -0,0 +1,36 @@
+package sec3.io.buffered;
+
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import static sec3.io.buffered.BufferedConst.*;
+
+public class CreateFileV2 {
+    public static void main(String[] args) throws IOException {
+        FileOutputStream fos = new FileOutputStream(FILE_NAME);
+        long startTime = System.currentTimeMillis();
+
+        byte[] buffer = new byte[BUFFER_SIZE];
+        int bufferIndex = 0;
+
+        for (int i = 0; i < FILE_SIZE; i++) {
+            buffer[bufferIndex++] = 1;
+
+            //버퍼가 가득 차면 쓰고, 버퍼를 비운다.
+            if (bufferIndex == BUFFER_SIZE) {
+                fos.write(buffer);
+                bufferIndex = 0;
+            }
+        }
+
+        if (bufferIndex > 0) {
+            fos.write(buffer, 0, bufferIndex);
+        }
+        long endTime = System.currentTimeMillis();
+        System.out.println("File created : " + FILE_NAME);
+        System.out.println("File size : " + FILE_SIZE / 1024 / 1024);
+        System.out.println("Time taken : " + (endTime - startTime) + " ms ");
+
+    }
+
+}
